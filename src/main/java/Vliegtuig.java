5dba5a7bfdb89ffe368be5abfe85f5cee9750105
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-05-15 17:30:49 +0200

    add Vliegtuig

--- src/main/java/Vliegtuig.java
+++ src/main/java/Vliegtuig.java
@@ -1,2 +1,31 @@
 public class Vliegtuig {
+    private String naam;
+    private int tegenwind, passagiers;
+    private boolean isNederlands;
+    private Lading lading;
+
+    public Vliegtuig(String naam, int tegenwind, int passagiers, boolean isNederlands, Lading lading) {
+        this.naam = naam;
+        this.tegenwind = tegenwind;
+        this.passagiers = passagiers;
+        this.isNederlands = isNederlands;
+        this.lading = lading;
+    }
+
+    public boolean opstijgen(Luchthaven luchthaven) {
+        return true;
+    }
+
+    private boolean vraagToestemming(Verkeerstoren verkeerstoren) {
+        return verkeerstoren.geefAntwoord();
+    }
+
+    private int meetTegenWind(int tegenwind) {
+        this.tegenwind = tegenwind;
+        return this.tegenwind;
+    }
+
+    public void landen(Luchthaven luchthaven) {
+
+    }
 }

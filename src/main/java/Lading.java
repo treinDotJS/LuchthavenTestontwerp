78c84ebe8028eebed8dcec54b84ae319ecfca658
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-05-15 17:22:50 +0200

    add Lading

--- src/main/java/Lading.java
+++ src/main/java/Lading.java
@@ -1,2 +1,17 @@
 public class Lading {
+    private int gewicht;
+    private boolean afhandelenDoorHaven;
+
+    public Lading(int gewicht, boolean afhandelenDoorHaven) {
+        this.gewicht = gewicht;
+        this.afhandelenDoorHaven = afhandelenDoorHaven;
+    }
+
+    public int getGewicht() {
+        return gewicht;
+    }
+
+    public boolean getAfhandelenDoorHaven() {
+        return afhandelenDoorHaven;
+    }
 }
